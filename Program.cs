d072ad036004f18cc06611299749e12250a066a6
kwt1326 <u1326@hotmail.com>
2020-06-03 13:35:18 +0900

    api server created

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace asp_net_practice
+namespace dotTestApiServer
 {
     public class Program
     {
