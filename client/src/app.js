56161b6f7f4beb64a697c0824ed4bfd6fbd7ea7b
kwt1326 <u1326@hotmail.com>
2020-06-04 17:33:40 +0900

    test weather api connected react <-> webapi

--- client/src/app.js
+++ client/src/app.js
@@ -1,11 +1,65 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import axios from 'axios';
 
 class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      text: this.props.data.text
+      text: this.props.data.text,
+      weather: [], // test call api
+    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      text: 'Client Loaded',
+    })
+    this.callApiWeather();
+  }
+
+  callApiWeather = async () => {
+    await axios({
+      method: 'GET',
+      url: 'https://localhost:5001/WeatherForecast',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      withCredentials: true,
+    }).then(res => {
+      console.log(res)
+      this.setState({ weather: res.data })
+    }).catch(err => {
+      console.log(err)
+    })
+  }
+
+  renderWeatherTable = () => {
+    const { weather } = this.state;
+    if (!!weather) {
+      return (
+        <table>
+          <thead>
+            <tr>
+              <td style={{ padding: '10px' }}>
+                Random Weather
+              </td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              weather.map(elem => {
+                return (<tr>
+                  <td style={{ padding: '10px' }}>{elem.date}</td>
+                  <td style={{ padding: '10px' }}>{elem.summary}</td>
+                  <td style={{ padding: '10px' }}>{elem.temperatureC}</td>
+                  <td style={{ padding: '10px' }}>{elem.temperatureF}</td>
+                </tr>)
+              })
+            }
+          </tbody>
+        </table>
+      )
     }
   }
 
@@ -13,14 +67,12 @@ class App extends Component {
     return (
       <div>
         <h1>{this.state.text}</h1>
+        {
+          this.state.weather
+          && this.renderWeatherTable()
+        }
       </div>
     );
-  }
-
-  componentDidMount() {
-    this.setState({
-      text: 'Client Loaded'
-    })
   }
 }
 
--- server/Startup.cs
+++ server/Startup.cs
@@ -15,6 +15,8 @@ namespace dotTestApiServer
 {
     public class Startup
     {
+        readonly string AllowSpecficOrigins = "!@allow&&speci#fic$$Origi^ns";
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -25,6 +27,15 @@ namespace dotTestApiServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options => 
+            {
+                options.AddPolicy(name: AllowSpecficOrigins,
+                builder => 
+                {
+                    builder.AllowCredentials();
+                    builder.WithOrigins("http://localhost:3000");
+                });
+            });
             services.AddControllers();
         }
 
@@ -39,6 +50,8 @@ namespace dotTestApiServer
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(AllowSpecficOrigins);
 
             app.UseAuthorization();
 
