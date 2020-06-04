38d78c05392c8b0ec643b68c7da4d510c438f85a
kwt1326 <u1326@hotmail.com>
2020-06-04 14:55:30 +0900

    react ssr created

---
+++ client/server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
+import App from '../src/app';
+import wdm from './wdm'
+import RenderHtml from './renderHtml';
+
+const app = express();
+const port = 3000;
+
+app.use(wdm);
+
+app.get('/', function (req, res, next) {
+
+  let preloadState = {
+    text: 'Server-Side Rendering'
+  };
+
+  let renderProps = {
+    preloadState: `window.__PRELOADED_STATE__ =${JSON.stringify(preloadState).replace(/</g, '\\u003c')}`,
+    script: '/build/client.bundle.js'
+  };
+
+  ReactDOMServer.renderToNodeStream(
+    <RenderHtml {...renderProps}>
+      <App data={preloadState} />
+    </RenderHtml>
+  ).pipe(res);
+});
+
+app.listen(port, () => {
+  console.log('http://localhost:3000')
+});
